29ab2c03a08e87dc7d1150922df701d4f55dcbda
Sk1nnyJimmy <had21010@byui.edu>
2023-01-18 16:07:27 -0700

    Completed Learning 02 lesson

--- prepare/Learning02/Program.cs
+++ prepare/Learning02/Program.cs
@@ -4,6 +4,24 @@ class Program
 {
     static void Main(string[] args)
     {
-        Console.WriteLine("Hello Learning02 World!");
+        Job job1 = new Job();
+        job1._jobTitle = "Teller/Customer Service Rep.";
+        job1._company = "America First Credit Union";
+        job1._startYear = 2017;
+        job1._endYear = 2020;
+
+        Job job2 = new Job();
+        job2._jobTitle = "Inventory Control Specialist";
+        job2._company = "Nutraceutical/Better Being Co.";
+        job2._startYear = 2020;
+        job2._endYear = 2023;
+
+        Resume myResume = new Resume();
+        myResume._name = "Robert Hadley";
+
+        myResume._jobs.Add(job1);
+        myResume._jobs.Add(job2);
+
+        myResume.Display();
     }
 }
